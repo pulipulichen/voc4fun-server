c680f2ebd9381d64fed8aa439a74e75bd2e34be6
Pulipuli Chen <pulipuli.chen@gmail.com>
2015-12-04 23:33:48 +0800

    1204 learn_flashcard.html的讀取其他人標註的功能有待實作：需等待log同步功能完成

--- model/note.php
+++ model/note.php
@@ -28,5 +28,24 @@ if (isset($_GET) && count($_GET) > 0 && isset($_GET["q"]) && isset($_GET["uuid"]
     if (is_null($notes)) {
         $notes = array();
     }
+    
+    $notes = array(
+        array(
+            "uuid"=> 1,
+            "name"=> "模擬同學A",
+            "note"=> "心得A\n\n\n\n\n\n心得心得"
+        ),
+        array(
+            "uuid"=> 2,
+            "name"=> "模擬同學B",
+            "note"=> "心得B\n\n\n\n\n\n心得心得"
+        ),
+        array(
+            "uuid"=> 3,
+            "name"=> "模擬同學C",
+            "note"=> "心得C\n\n\n\n\n\n心得心得"
+        )
+    );
+    
     jsonp_callback($notes);
 }
